92721a7e4af1c1cf219dc07d0067a3e2efc872c7
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-10-13 16:36:02 +0900

    updated seed + added documentation

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development, :test do
   gem 'capistrano-rails'
   gem 'capistrano-rbenv'
   gem 'capistrano-passenger'
+  gem 'rails-erd'
 end
 
 group :development do
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+University.create!([
+		{:university_name => "UP Baguio", :university_code => "UPB"},
+		{:university_name => "UP Cebu", :university_code => "UPC"},
+		{:university_name => "UP Diliman", :university_code => "UPD"},
+		{:university_name => "UP Los Banos", :university_code => "UPL"},
+		{:university_name => "UP Manila", :university_code => "UPM"},
+		{:university_name => "UP Mindanao", :university_code => "UPN"},
+		{:university_name => "UP Open University", :university_code => "UPO"},
+		{:university_name => "UP PGH", :university_code => "UPP"},
+		{:university_name => "UP System", :university_code => "UPS"},
+		{:university_name => "UP Visayas", :university_code => "UPV"},
+	])
